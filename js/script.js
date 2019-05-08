689c5dd8324c644167d44d46cff763d2ec3065bd
Your Name <hazara.builder@gmail.com>
2019-05-08 14:33:54 +0200

    deleted unwanted images and moved J.S codes in J.S folder

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-let age = 25;
-console.log(age !=='25')
+function slideshow() {
+  var x = document.getElementById('check-class');
+  if (x.style.display === "none") {
+      x.style.display = "inline-block";
+  } else {
+      x.style.display = "none";
+  }
+}
